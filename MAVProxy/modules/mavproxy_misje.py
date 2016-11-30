c0f60b203f263c5b1bcd7995f21fd6c4308e70b3
Krzysiu <krzysiucwian@gmail.com>
2016-11-30 11:55:14 +0100

    Przeniesiono bibliotekę interop bezpośredio do folderu MAVProxy

--- MAVProxy/modules/mavproxy_misje.py
+++ MAVProxy/modules/mavproxy_misje.py
@@ -1,44 +1,53 @@
-'''module template'''
-import time, math
+import time, math, json
 from pymavlink import mavutil
 from MAVProxy.modules.lib import mp_module
 from MAVProxy.modules.lib.mp_settings import MPSetting
 
+# Sciezka do biblioteki interop
 import sys
 #sys.path.append('/home/krzysiu/interop/client/tools')
 sys.path.append('/home/krzysiu/MAVProxy/interop')
+sys.path.append('/usr/lib/python2.7')
+sys.path.append('/usr/lib/python3.5')
 
 INTEROP_URL = 'http://localhost:8000'
-INTEROP_USERNAME = 'testuser'
+INTEROP_USERNAME = 'testuser2'
 INTEROP_PASSWORD = 'testpass'
 POLL_SEC = 0.1
 PRINT_SEC = 3
 
+
 try:
-    from interop import AsyncClient
+    from interop import AsyncClient	
     from interop import Telemetry
+    import interop
 except ImportError as e:
     raise ImportError(
         'Failed to import interop libraries. Have you added the libs to '
         'the path? Error: %s' % e)
 
 
+client = interop.AsyncClient(url=INTEROP_URL,username=INTEROP_USERNAME,password=INTEROP_PASSWORD)
+mission = client.get_missions()
+#stationary_obstacles, moving_obstacles = client.get_obstacles()
 
-class Telemetria(mp_module.MPModule):
+print len(mission.result())
+for a in (mission.result()):
+	print a
+
+
+class Misje(mp_module.MPModule):
   def __init__(self, mpstate):
-    super(Telemetria, self).__init__(mpstate, "telemetria", "telemetria module")
-    self.add_command('zrobCos', self.cmd_zrobCos, "zrobCos commands")
+    super(Misje, self).__init__(mpstate, "misje", "misje module")
     self.client = AsyncClient(INTEROP_URL, INTEROP_USERNAME, INTEROP_PASSWORD)
 
   def mavlink_packet(self, m):
 	'''handle a mavlink packet'''
-	if m.get_type() == 'GLOBAL_POSITION_INT':
-		telemetry = Telemetry(float(m.lat)/10000000, float(m.lon)/10000000, float(m.alt)/1000, float(m.hdg)/100)
-		self.client.post_telemetry(telemetry)		
+		
 
-  def cmd_zrobCos(self,args):
-	print "Niby co"
+
 
 def init(mpstate):
-	'''initialise module'''
-	return Telemetria(mpstate)
+	missions = client.get_missions()
+	print missions
+	return Misje(mpstate)
